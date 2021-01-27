4efd2b35f24ebcd1c4ec4480f3825b470ec25bd0
areenjaradat <areenjaradat96@gmail.com>
2021-01-27 16:17:27 +0200

    modify app.js file

--- app.js
+++ app.js
@@ -1,15 +1,17 @@
 
 
 var maldives = prompt('what do you want to visit in the maldives put on of this choices Maafushi or Biyadhoo or Fihalhohi to have more info about this place ');
-
+document.write('<h1>'+maldives+'</h1>')
 if (maldives=='Maafushi'){
 document.write('<h4>'+'<em>'+'Maafushi is one of the biggest and most popular local islands in Maldives. It is located in South Male Atoll just 27 km away from Male. The size is 1270 x 265 m. The island itself has an elongated form. 2700 people live on the island. It’s amazing how so many people can live on such a small piece of land, but as on other islands, you won’t feel that the place is overcrowded.'+'</em>'+'</h4>');
 
-//<img src="images/Maafushi.jpg">
+document.write('<img src="images/Maafushi.jpg"/>')  
 }else if(maldives=='Biyadhoo'){
     document.write('<h4>'+'<em>'+'There’s no pretence about Biyadhoo – if you’re looking for a simple escape at a good value, established resort with a wonderful natural environment, this is it. It’s why many of the guests here return year after year. The resort has recently improved many of its facilities, with changes including free Wi-Fi, twice-weekly snorkelling trips, as well as a refreshment of the rooms, reception, restaurant and bar.'+'</em>'+'</h4>');
-    }else if (maldives=='Fihalhohi'){
+    document.write('<img src="images/Biyadhoo.jpg"/>')     
+}else if (maldives=='Fihalhohi'){
         document.write('<h4 font>'+'<em>'+'Come to a place where a captivating view of the Indian Ocean and the natural tropical beauty of the island is a constant setting. Fihalhohi Island Resort, Maldives is the absolute paradise island escape. A truly idyllic spot where mind and body are reinvigorating, and where the sands of time stand still.'+'</em>'+'</h4>');
+        document.write('<img src="images/Fihalhohi.jpg"/>') 
     }
     else{
         alert('wrong insert')
@@ -24,6 +26,7 @@ function changeBackground(color) {
  }
 if (color=='red'){
     window.addEventListener("load",function() { changeBackground('red') });
+     
 }else if(color=='yellow'){
     window.addEventListener("load",function() { changeBackground('yellow') });
 }else if(color=='blue'){
